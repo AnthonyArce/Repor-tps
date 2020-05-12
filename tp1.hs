63ac8b28af9128a4d4bfc5fba5445759017f982a
anthony Obregon <anthony_cai_1997@outlook.com>
2020-05-12 10:28:30 -0300

    se soluciono a medias el punto 1

--- tp1.hs
+++ tp1.hs
@@ -15,11 +15,29 @@ data Auto = Auto {
 } deriving Show
 
 auto1::Auto
-auto1 = Auto "AT001LN" [0, 1, 0, 0] 1500 40 (10,20,2019)
+auto1 = Auto {
+    patente = "AT001LN",
+    desgasteLlantas = [0, 1, 0, 0],
+    rpm = 1500,
+    temperaturaAgua = 40,
+    ultimoArreglo = (10,20,2019)
+    }
 
 auto2:: Auto
 auto2 = Auto "DJV214" [0, 1, 0, 0] 1500 40 (10,20,2019)
 
-costoArreglo::Auto -> Double
+auto3:: Auto
+auto3 = Auto "DJV216" [0, 1, 0, 0] 1500 40 (10,20,2019)
+
+
+
+primerasLetras::String->String
+primerasLetras patente = take 2 patente -- funcion take ("cantidad de letras a tomar") (String)
+
+costoArreglo::Auto -> Int
 costoArreglo (Auto patente _ _ _ _ ) | ((>6).length ) patente = 12500
-                                     | otherwise = 10000
+                                     | (primerasLetras patente == "DJ") && (last patente == '4') = length patente * 3000
+                                     | (primerasLetras patente == "DJ") && (last patente /= '4') = 20000
+                                     | otherwise = 15000
+
+
