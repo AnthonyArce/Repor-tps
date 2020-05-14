bd542eb174034436b8e3f930a043350101ab76de
anthony Obregon <anthony_cai_1997@outlook.com>
2020-05-13 21:04:21 -0300

    se modificaron las fechas de los autos

--- tp1.hs
+++ tp1.hs
@@ -17,18 +17,20 @@ data Auto = Auto {
 auto1::Auto
 auto1 = Auto {
     patente = "AT001LN",
-    desgasteLlantas = [0, 1, 0, 0],
+    desgasteLlantas = [0.51, 1, 0, 0],
     rpm = 1500,
     temperaturaAgua = 40,
     ultimoArreglo = (10,20,2019)
     }
 
 auto2:: Auto
-auto2 = Auto "DJV214" [0, 1, 0, 0] 1500 40 (10,20,2019)
+auto2 = Auto "DJV214" [0, 1, 0, 0] 1500 40 (10,20,2015)
 
 auto3:: Auto
-auto3 = Auto "DJV216" [0, 1, 0, 0] 1500 40 (10,20,2019)
+auto3 = Auto "DJV216" [0.5, 1, 0, 0] 3000 40 (10,20,2016)
 
+auto4:: Auto
+auto4 = Auto "DJV216" [0.4, 1, 0, 0] 1500 40 (10,20,2019)
 
 
 primerasLetras::String->String
@@ -41,3 +43,27 @@ costoArreglo (Auto patente _ _ _ _ ) | ((==6).length ) patente = 12500
                                      | otherwise = 15000
 
 
+autoPeligroso::Auto->Bool
+autoPeligroso (Auto _ desgasteLlantas _ _ _ ) | ((>=0.5) . head ) desgasteLlantas = True
+                                              | otherwise = False
+
+
+-- necesitaRevision::Auto->Bool
+-- necesitaRevision (Auto _ _ _ _ ( _, _, year) ) | year == 2016 = False
+--                                                | year == 2015 = True
+
+
+
+alfa::Auto->Int
+alfa (Auto _ _ rpm _ _) | rpm < 2000 = rpm
+                        | otherwise = 2000
+
+bravo::Auto->[Desgaste]         
+bravo (Auto _ desgasteLlantas _ _ _ ) =  [0 ,0 ,0 ,0 ]       
+
+-- 
+
+tomarElemento::(Int,Int) -> Int
+tomarElemento ( _ ,b ) = b
+-- zulu::Auto -> (Int,[Desgaste])
+-- zulu (Auto _ desgasteLlantas rpm _ _) = ( (alfa (Auto _ _ rpm _ _)),(bravo (Auto _ desgasteLlantas _ _ _) ) )
